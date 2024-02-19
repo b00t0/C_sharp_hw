13cbe53cb804e3acafa6144930f86b8fa27342a3
Igor Bobkov <bobkovid@gmail.com>
2024-02-19 23:34:45 +0300

    Completed task04hw1

---
+++ hw1/task04hw1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Внутри класса Answer напишите метод PrintEvenNumbers, которая на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), 
+// после каждого числа должен быть знак пробела.
+
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 2)
+{
+    Console.WriteLine("Incorrect input");
+    return;
+}
+
+int count = 0;
+while (count < number - 1)
+{
+    count = count + 2;
+    Console.Write($"{count} ");
+}
