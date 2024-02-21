928fbc2dd1919c715f175e8c35ff91a6e2aae248
Igor Bobkov <bobkovid@gmail.com>
2024-02-21 23:49:46 +0300

    Completed task02hw2

---
+++ hw2/task02hw2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 
+// и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Enter the x coordinates");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the y coordinates");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("The first quarter of the Cartesian system coordinates");
+    return;
+}
+
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("The second quarter of the Cartesian system coordinates");
+    return;
+}
+
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("The third quarter of the Cartesian system coordinates");
+    return;
+}
+
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("The fourth quarter of the Cartesian system coordinates");
+    return;
+}
