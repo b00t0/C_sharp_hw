0c54df9e25984f0dae11d5feb3231c1a78805256
Igor Bobkov <bobkovid@gmail.com>
2024-02-21 23:54:08 +0300

    Completed task03hw2

---
+++ hw2/task03hw2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+Console.WriteLine("Enter the integer natural two-digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 10 || number > 99) {
+        Console.WriteLine("Incorrect input");
+        return;
+    }
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    if (firstDigit > secondDigit) {
+        Console.WriteLine($"{firstDigit}");
+    }
+    else {
+Console.WriteLine($"{secondDigit}");
+    }
+    
