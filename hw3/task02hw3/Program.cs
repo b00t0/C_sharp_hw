cdf6f98c2f1df6ef8e7a1d62254510af1c55c135
Igor Bobkov <bobkovid@gmail.com>
2024-02-27 23:07:04 +0300

    Completed task02hw3

---
+++ hw3/task02hw3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив на 10 целых чисел. 
+// Напишите программу, которая определяет количество чётных чисел в массиве.
+
+Console.Clear();
+
+int CountEvenNumbers(int[] numbers)
+{
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+return count;
+}
+
+int[] array = {2, 5, 2, 3, 6, 3, 7, 8, 2, 4};
+
+Console.WriteLine(CountEvenNumbers(array));
