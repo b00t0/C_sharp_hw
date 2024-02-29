9e24a342213adbe5629a30a47493a875dc97aa32
Igor Bobkov <bobkovid@gmail.com>
2024-02-29 14:18:02 +0300

    Completed task01hw4

---
+++ hw4/task01hw4/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа 
+// с консоли. Программа завершается при вводе символа ‘q’ 
+// или при вводе числа, сумма цифр которого чётная.
+
+Console.Clear();
+
+int DigitSum(int currentNum)
+{
+    int digitSum = 0;
+    while (currentNum > 0)
+    {
+        digitSum = digitSum + currentNum % 10;
+        currentNum = currentNum / 10;
+    }
+    return digitSum;
+}
+
+
+while (true)
+{
+    Console.WriteLine("Enter the integer number");
+    string stopWord = Console.ReadLine();
+
+    if (stopWord == "q")
+    {
+        Console.WriteLine("Stop word");
+        break;
+    }
+
+    int number;
+    if (int.TryParse(stopWord, out number))
+    {
+        // int sum = 0;
+        int sum = DigitSum(number);
+        // while (number > 0)
+        // {
+        //     sum += number % 10;
+        //     number /= 10;
+        // }
+        Console.WriteLine($"Sum of the numbers is {sum}");
+
+        if (sum % 2 == 0)
+        {
+            Console.WriteLine("The digitSum is even");
+            break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Incorrect input");
+    }
+}
+
