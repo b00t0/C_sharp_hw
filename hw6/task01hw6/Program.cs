10d135ac627e9902915b8b097fcfe8d388fa33aa
Igor Bobkov <bobkovid@gmail.com>
2024-03-07 20:51:25 +0300

    Completed task01hw6

---
+++ hw6/task01hw6/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+
+string GetStringFromCharMatrix(char[,] matrix)
+{
+    string str = string.Empty;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            str += matrix[i, j];
+        }
+    }
+    return str;
+}
+
+char[,] charsMatrix = {
+    { 'a', 'b', 'c' },
+    { 'd', 'e', 'f' }
+};
+
+Console.WriteLine(GetStringFromCharMatrix(charsMatrix));
