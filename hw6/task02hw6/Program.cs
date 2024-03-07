fdc581663280943b5a7d71ddad449122dbaded4d
Igor Bobkov <bobkovid@gmail.com>
2024-03-07 20:51:40 +0300

    Completed task02hw6

---
+++ hw6/task02hw6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+Console.Clear();
+
+Console.WriteLine("Enter the string");
+string str = Console.ReadLine()!;
+
+string lowers =str.ToLower();
+Console.WriteLine(lowers);
